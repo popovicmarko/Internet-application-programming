47a13f4b5413b337d77ffa77422ac3a0777c183f
Marko Popovic <104674760+popovicmarko@users.noreply.github.com>
2022-12-15 15:17:12 +0100

    Update app.js

--- classes-js/app.js
+++ classes-js/app.js
@@ -31,7 +31,7 @@ class Knjiga {
 }
 
 
-Lorem_ipsum = new Knjiga("Lorem Ipsum", 123, 2022, "Marko Popovic", "DUNP", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "https://www.stamparija.rs/images/stories/slike/knjige-stapmarija%20proof%20beograd.jpg");
+Lorem_ipsum = new Knjiga("Lorem Ipsum", 123, 2022, "Cirivoje Cicoo", "DUNP", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "https://www.stamparija.rs/images/stories/slike/knjige-stapmarija%20proof%20beograd.jpg");
 
 Lorem_ipsum.render("book");
 Lorem_ipsum.render("book2");
